4bd5136e2264511bb7b57d4b5ccf2798c85ad174
Thamilselvan R <tamilselvan02042005@gmail.com>
2025-09-04 18:41:30 +0530

    COMMIT

---
+++ script.js
@@ -0,0 +1,10 @@
+var bulb=document.getElementById("bulb")
+        var cat=document.getElementById("cat")
+        function turnOn(){
+          bulb.src= "https://ik.imagekit.io/4z8covdo9/Full%20stack/bulb-go-on-img.png";
+          cat.src="https://ik.imagekit.io/4z8covdo9/Full%20stack/cat-img.png";
+        }
+        function turnOff() {
+      bulb.src = "https://ik.imagekit.io/4z8covdo9/Full%20stack/bulb-go-off-img.png";
+      cat.src = "https://ik.imagekit.io/4z8covdo9/Full%20stack/cat-eyes-img.png"; 
+    }
